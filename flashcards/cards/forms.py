c9d23be9ce50f19ff6e7289a7dba3017e1c90991
Jenna Dow <jlynnedow@gmail.com>
2026-08-19 11:18:03 -0400

    added free-form response for answers

--- flashcards/cards/forms.py
+++ flashcards/cards/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class CardCheckForm(forms.Form):
     card_id = forms.IntegerField(required=True)
-    solved = forms.BooleanField(required=False)
+    user_answer = forms.CharField(max_length=100, label='User Answer')
